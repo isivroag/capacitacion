252d861981b96ad26deaaf8f84a6f334bfa911e7
isivroag <isivroag@gmail.com>
2023-09-15 15:01:22 -0600

    I 15SEP23 3:00PM

---
+++ fjs/formulario_ivan.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+
+    tabla = $("#tabla").DataTable({
+        "columnDefs": [{
+                "targets": -1,
+                "data": null,
+                "defaultContent": "<div class='text-center'><div class='btn-group'><button class='btn btn-sm btn-primary  btnEditar'><i class='fas fa-edit'></i></button><button class='btn btn-sm btn-danger btnBorrar'><i class='fas fa-trash-alt'></i></button></div></div>"
+            },
+        
+         
+
+        ],
+
+        //Para cambiar el lenguaje a español
+        "language": {
+            "lengthMenu": "Mostrar _MENU_ registros",
+            "zeroRecords": "No se encontraron resultados",
+            "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+            "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+            "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+            "sSearch": "Buscar:",
+            "oPaginate": {
+                "sFirst": "Primero",
+                "sLast": "Último",
+                "sNext": "Siguiente",
+                "sPrevious": "Anterior"
+            },
+            "sProcessing": "Procesando...",
+        }
+    });
+
+})
--- ivan/formulario_ivan.php
+++ ivan/formulario_ivan.php
@@ -10,12 +10,13 @@ $fechaini = (isset($_GET['fechaini'])) ? $_GET['fechaini'] : date('Y-m-d');
 
 
 <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
-<!-- Ionicons -->
 <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-<!-- overlayScrollbars -->
 <link rel="stylesheet" href="../css/adminlte.css">
 <link rel="stylesheet" href="../css/estilo.css?v=<?php echo (rand()); ?>">
 <script src="https://kit.fontawesome.com/f1fe472df3.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="../plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="../plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+<link rel="stylesheet" href="../css/estilo.css">
 
 <!DOCTYPE html>
 <html lang="en">
@@ -35,7 +36,10 @@ $fechaini = (isset($_GET['fechaini'])) ? $_GET['fechaini'] : date('Y-m-d');
                 <div class="col-md-6">
                     <div class="card">
                         <div class="card-header bg-navy">
-                            <h3 class="card-title">Cotizador</h3>
+                            <div class="row justify-content-center">
+                                <h3 class="card-title">Cotizador</h3>
+                            </div>
+                          
                         </div>
                         <div class="card-body">
 
@@ -79,7 +83,7 @@ $fechaini = (isset($_GET['fechaini'])) ? $_GET['fechaini'] : date('Y-m-d');
                             </form>
                             <?php if ($vt != 0) {
                             ?>
-                                <table class="table table-borderer table-sm table-condensed table-hover">
+                                <table class="table table-borderer table-sm table-condensed table-hover" name="tabla" id="tabla">
                                     <thead class="bg-gradient-navy">
                                         <tr>
                                             <th class="text-center">MESUALIDAD</th>
@@ -127,21 +131,29 @@ $fechaini = (isset($_GET['fechaini'])) ? $_GET['fechaini'] : date('Y-m-d');
 
 </body>
 
-</html>
 
-<!-- jQuery -->
+
 <script src="../plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
+<script src="../fjs/formulario_ivan.js?v=<?php echo (rand()); ?>"></script>
 <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-<!-- DataTables -->
 <script src="../plugins/datatables/jquery.dataTables.min.js"></script>
 <script src="../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
 <script src="../plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
 <script src="../plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-
-
-<!-- AdminLTE App -->
 <script src="../js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
 <script src="../js/demo.js"></script>
+
+
+
+
+
+<script src="../plugins/sweetalert2/sweetalert2.all.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/plug-ins/1.10.21/sorting/formatted-numbers.js"></script>
+
+</html>
+
