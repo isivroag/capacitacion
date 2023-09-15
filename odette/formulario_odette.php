48eba096cdd1e12de3b1c2e7193cf635448a36b2
odettu <odette.chimal@gmail.com>
2023-09-15 08:22:21 -0600

    O 15SEP23 8:22AM

--- odette/formulario_odette.php
+++ odette/formulario_odette.php
@@ -1,7 +1,11 @@
 <?php
 
+date_default_timezone_set('America/Mexico_City');
+
 $valter = (isset($_POST["valter"])) ? $_POST["valter"] : 0;
 $nmens = (isset($_POST["nmens"])) ? $_POST["nmens"] : 0;
+$tinteres = (isset($_POST["tinteres"])) ? $_POST["tinteres"] : 0;
+$fechaini = (isset($_POST["fechaini"])) ? $_POST["fechaini"] : date('d-m-Y');
 
 ?>
 
@@ -25,69 +29,115 @@ $nmens = (isset($_POST["nmens"])) ? $_POST["nmens"] : 0;
 
 <body>
 
-    <?php if ($valter == 0) { ?>
-        <form method="post">
-            <label for="valter"> VALOR TERRENO:</label>
-            <input name="valter" id="valter" type="text"> <br><br>
-            <label for="nmens">NUMERO DE MENSUALIDADES:</label>
-            <input name="nmens" id="nmens" type="text"> <br><br>
-            <input type="submit" value="Enviar">
+    <section>
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row justify-content-center">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header bg-primary">
+                                <h3 class="card-title justify-content-center">COTIZADOR</h3>
+                            </div>
+                            <div class="card-body">
+                                <form method="post">
+                                    <div class="row justify-content-center">
+
+                                        <div class="col-sm-3">
+                                            <div class="form-group">
+                                                <label for="valter"> VALOR TERRENO:</label>
+                                                <input name="valter" class="form-control" id="valter" type="text" value="<?php echo $valter ?>" placeholder="Valor Terreno"> <br><br>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-sm-3">
+                                            <div class="form-group">
+                                                <label for="nmens"> NO. MENSUALIDADES:</label>
+                                                <input name="nmens" class="form-control" id="nmens" type="number" value="<?php echo $nmens ?>" placeholder="No. Mensualidades"> <br><br>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-sm-3">
+                                            <div class="form-group">
+                                                <label for="tinteres"> TASA DE INTERES:</label>
+                                                <input name="tinteres" class="form-control" id="tinteres" type="text" value="<?php echo $tinteres ?>" placeholder="Tasa de Interes"> <br><br>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-sm-3">
+                                            <div class="form-group">
+                                                <label for="fechaini"> FECHA DE INICIO:</label>
+                                                <input name="fechaini" class="form-control" id="fechaini" type="date" value="<?php echo $fechaini ?>" placeholder="Fecha de Inicio"> <br><br>
+                                            </div>
+                                        </div>
+
+                                        <div class="row justify-content-center">
+                                            <div class="col-sm-12">
+                                                <input type="submit" class="btn-primary btn-block" value="CALCULAR">
+                                            </div>
+                                        </div>
+
+                                </form>
+                                <?php if ($valter != 0) { ?>
+
+                                    <table class="table table-borderer table-condensed table-hover">
+                                        <thead>
+                                            <tr>
+                                                <th>MENSUALIDAD</th>
+                                                <th>FECHA</th>
+                                                <th>MONTO</th>
+                                                <th>CAPITAL</th>
+                                                <th>INTERES</th>
+                                                <th>TOTAL</th>
+                                                <th>SALDO</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+
+                                            <?php
+
+                                            $monto = $valter;
+                                            $tasa = 16.15;
+                                            $mensualidad = $monto / $nmens;
+                                            $interes = ($tasa / 100) / 12;
+                                            $fecha1 = date("d-m-Y");
+                                            $saldo = $monto;
+
+                                            for ($i = 1; $i <= $nmens; $i++) {
+
+                                                //Inician los calculos
+                                                $minteres = $saldo * $interes;
+                                                $saldo = $saldo - $mensualidad;
+                                            ?>
+                                                <tr>
+
+                                                    <td><?php echo $i ?></td>
+                                                    <td><?php echo $fecha1 ?></td>
+                                                    <td><?php echo number_format($monto, 2) ?></td>
+                                                    <td><?php echo number_format($mensualidad, 2) ?></td>
+                                                    <td><?php echo number_format($minteres, 2) ?></td>
+                                                    <td><?php echo number_format($mensualidad + $minteres, 2) ?></td>
+                                                    <td><?php echo number_format($saldo, 2) ?></td>
+
+                                                </tr>
+
+                                        <?php
+                                                $fecha1 = date("d-m-Y", strtotime($fecha1 . "+1 month"));
+                                            }
+                                        } ?>
+
+                                        </tbody>
+                                    </table>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
 
 
-        </form>
-
-    <?php
-    } else {
-    ?>
-        <table class="table table-borderer table-condensed table-hover"> 
-            <thead>
-                <tr>
-                    <th>MENSUALIDAD</th>
-                    <th>FECHA</th>
-                    <th>MONTO</th>
-                    <th>CAPITAL</th>
-                    <th>INTERES</th>
-                    <th>TOTAL</th>
-                    <th>SALDO</th>
-                </tr>
-            </thead>
-            <tbody>
-
-                <?php
-
-                $monto = $valter;
-                $tasa = 16.15;
-                $mensualidad = $monto / $nmens;
-                $interes = ($tasa / 100) / 12;
-                $fecha1 = date("d-m-Y");
-                $saldo = $monto;
-
-                for ($i = 1; $i <= $nmens; $i++) {
-
-                    //Inician los calculos
-                    $minteres = $saldo * $interes;
-                    $saldo = $saldo - $mensualidad;
-                ?>
-                    <tr>
-
-                        <td><?php echo $i ?></td>
-                        <td><?php echo $fecha1 ?></td>
-                        <td><?php echo number_format($monto, 2) ?></td>
-                        <td><?php echo number_format($mensualidad, 2) ?></td>
-                        <td><?php echo number_format($minteres, 2) ?></td>
-                        <td><?php echo number_format($mensualidad + $minteres, 2) ?></td>
-                        <td><?php echo number_format($saldo, 2) ?></td>
-
-                    </tr>
-
-            <?php
-                    $fecha1 = date("d-m-Y", strtotime($fecha1 . "+1 month"));
-                }
-            } ?>
-
-            </tbody>
-
-        </table>
 
 </body>
 
